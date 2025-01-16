ade8a05bd8d3bcd7479b636254620a3baad90f46
Tortugon09 <axelramos001@gmail.com>
2025-01-16 12:22:28 -0600

    second commit app modificado

--- ui/app.py
+++ ui/app.py
@@ -4,9 +4,14 @@ import subprocess
 import os
 def run_spider(spider_name, output_file):
     try:
-        scrapy_project_path = "C:/Users/Axel/Desktop/ScraperPrueba/scrapers"
-        output_path = f"C:/Users/Axel/Desktop/ScraperPrueba/output/{spider_name}.csv"
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        current_dir = os.path.dirname(current_dir) 
+
+        scrapy_project_path = os.path.join(current_dir, "scrapers")
+
         os.chdir(scrapy_project_path)
+
+        output_path = os.path.join(current_dir, "output", f"{spider_name}.csv")
         command = ['py', '-m', 'scrapy', 'crawl', spider_name, '-o', output_path]
         try:
             subprocess.run(command, check=True)
